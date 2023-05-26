145582b7ccc66e49a652d9dbf914dc91aba4a16c
Dalbee <dalbertonyebuchi@gmail.com>
2023-05-26 18:00:20 +0300

    test run

---
+++ src/test/java/Step_Definition/HomePage.java
@@ -0,0 +1,25 @@
+package Step_Definition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePage {
+    public WebDriver driver;
+    @Given("^I am on the Home page$")
+    public void i_am_on_the_Home_page() throws Throwable {
+        System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+    }
+
+    @Then("^I should see the application logo$")
+    public void i_should_see_the_application_logo() throws Throwable {
+        driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[1]/img")).isDisplayed();
+
+    }
+}
---
+++ src/test/resources/HomePage.feature
@@ -0,0 +1,5 @@
+Feature: Home page  /* Requirements*/
+
+  Scenario: Verify that application logo is displayed on the home page. /* test case*/
+    Given I am on the Home page
+    Then I should see the application logo
