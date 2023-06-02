af94fecbfe47fc177cdfd6e624b7da36883d1c0e
Dalbee <dalbertonyebuchi@gmail.com>
2023-06-02 18:50:36 +0300

    Modify HomePage.java

--- src/test/java/Step_Definition/HomePage.java
+++ src/test/java/Step_Definition/HomePage.java
@@ -12,8 +12,9 @@ public class HomePage {
     public void i_am_on_the_Home_page() throws Throwable {
         System.setProperty("web-driver.chrome.driver", "c://chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        //driver.manage().window().maximize();
+        //driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         Thread.sleep(2000); //wait for 2 seconds or 2000 milliseconds//
 
     }
@@ -23,5 +24,6 @@ public class HomePage {
         driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[1]/img")).isDisplayed();
         driver.findElement(By.cssSelector("#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-branding > img")).isDisplayed();
 
+        driver.quit();
     }
 }
