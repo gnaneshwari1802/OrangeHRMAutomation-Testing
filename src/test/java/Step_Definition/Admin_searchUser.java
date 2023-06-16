1065c0c268b9103774a97734be41f15267a51e80
Dalbee <dalbertonyebuchi@gmail.com>
2023-06-16 14:59:23 +0300

    Modify Admin_Pages.java
    Add more code to Page Object Model

--- src/test/java/Step_Definition/Admin_searchUser.java
+++ src/test/java/Step_Definition/Admin_searchUser.java
@@ -5,6 +5,8 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+//import org.junit.Assert;
+//import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
