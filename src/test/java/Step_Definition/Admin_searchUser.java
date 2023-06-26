1c6ac026ed627b6aaa098012848e33fd78b8ac8c
Dalbee <dalbertonyebuchi@gmail.com>
2023-06-26 14:01:00 +0300

    Modify Admin_searchUser.java

--- src/test/java/Step_Definition/Admin_searchUser.java
+++ src/test/java/Step_Definition/Admin_searchUser.java
@@ -2,27 +2,24 @@ package Step_Definition;
 
 import Pages.Admin_Pages;
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class Admin_searchUser {
 
     public WebDriver driver;
-
-    /**
-     * //Commented out because we have created a Background/Pre-condition in the Admin_searchUser.feature file
-    @Given("^I am logged in$") **/
-    /**
-     * //Commented out because we have created a Background/Pre-condition in the Admin_searchUser.feature file
+    @Given("^I am logged in$")
     public void iAmLoggedIn() throws InterruptedException {
-
         System.setProperty("web-driver.chrome.driver", "c://chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-
         Thread.sleep(2000); //wait for 2 seconds or 2000 milliseconds//
 
         Admin_Pages page = PageFactory.initElements(driver, Admin_Pages.class);
@@ -30,18 +27,12 @@ public class Admin_searchUser {
         page.enterLoginPassword();
         page.clickLoginButton();
 
-        **/
-
-        /** //Commented out because it has been now declared in the Page Object Model file : Admin.Pages.java //
-         driver.findElement(By.name("username")).sendKeys("Admin");
-         driver.findElement(By.name("password")).sendKeys("admin123");
-         driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click(); // LoginButton
-
-
-        Thread.sleep(3000);
-
+        //Commented out because it has been now declared in the Page Object Model file : Admin.Pages.java //
+        //driver.findElement(By.name("username")).sendKeys("Admin");
+        //driver.findElement(By.name("password")).sendKeys("admin123");
+        //driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click(); // LoginButton
+        Thread.sleep(5000);
     }
-         **/
 
     @When("^I click the admin menu$")
     public void iClickTheAdminMenu() throws InterruptedException {
@@ -49,12 +40,10 @@ public class Admin_searchUser {
         page.clickAdminMenu();
         //Commented out because it has been now declared in the Page Object Model file : Admin.Pages.java //
         //driver.findElement(By.xpath("//span[text() = 'Admin']")).click(); //AdminMenuButton
-        Thread.sleep(3000);
-
+        Thread.sleep(2000);
         page.closeMenu();
         //Commented out because it has been now declared in the Page Object Model file : Admin.Pages.java //
         //driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/aside/nav/div[2]/div/div/button")).click(); // MenuToggleButton MinimizeMenuBar
-
     }
 
     @Then("^I should see the users page$")
@@ -71,23 +60,19 @@ public class Admin_searchUser {
     public void iEnterUserData() throws InterruptedException {
         Admin_Pages page = PageFactory.initElements(driver, Admin_Pages.class);
         page.searchByUsername();
-        Thread.sleep(2000);
         //Commented out because it has been now declared in the Page Object Model file : Admin.Pages.java //
         //driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/input")).sendKeys("Admin"); // UserNameSearchField text field for Username
-
-        /**
         Thread.sleep(2000);
-        new Select(driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/div/div/div[1]"))).selectByVisibleText("ESS");  // because this is a drop-down field for User Role
+
+        //new Select(driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/div/div/div[1]"))).selectByVisibleText("ESS");  // because this is a drop-down field for User Role
         // ERROR-:- org.openqa.selenium.support.ui.UnexpectedTagNameException: Element should have been "select" but was "div"
         // Select function cannot work because of bad web design. There is no "select" inside the DOM. Only a "div" is present. We cannot use the Select property. To resolve this, click on the drop-down menu and then click on the field you want to select.
         //driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/div/div/div[1]")).click();
-
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[3]/div/div[2]/div/div/input")).sendKeys("Admin"); // text field for Employee Name
-        Thread.sleep(2000);
-        new Select(driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[4]/div/div[2]/div/div/div[1]"))).selectByVisibleText("Enabled");  // because this is a drop-down field for Status
-        **/
-
+        //Thread.sleep(2000);
+        //driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[3]/div/div[2]/div/div/input")).sendKeys("Admin"); // text field for Employee Name
+        //Thread.sleep(2000);
+        //new Select(driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[4]/div/div[2]/div/div/div[1]"))).selectByVisibleText("Enabled");  // because this is a drop-down field for Status
+        //Thread.sleep(2000);
     }
 
     @And("^click on the search button$")
@@ -103,31 +88,29 @@ public class Admin_searchUser {
     public void searchResultsShouldReturnTheCorrectUser() {
         Admin_Pages page = PageFactory.initElements(driver, Admin_Pages.class);
         page.AssertSearchResult();
-        /** //Commented out because it has been now declared in the Page Object Model file : Admin.Pages.java //
-        String ExpectedReturnedUsername = "Admin";        // UserNameColumn
-        String ActualReturnedUsername = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[2]/div")).getText();
-        Assert.assertEquals(ExpectedReturnedUsername, ActualReturnedUsername);
-        System.out.println(ActualReturnedUsername);
+        //Commented out because it has been now declared in the Page Object Model file : Admin.Pages.java //
+        //String ExpectedReturnedUsername = "Admin";        // UserNameColumn
+        //String ActualReturnedUsername = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[2]/div")).getText();
+        //Assert.assertEquals(ExpectedReturnedUsername, ActualReturnedUsername);
+        //System.out.println(ActualReturnedUsername);
 
-        String ExpectedReturnedUserRole = "Admin";     // UserRoleColumn
-        String ActualReturnedUserRole = driver.findElement(By.xpath("//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[3]/div")).getText();
-        Assert.assertEquals(ExpectedReturnedUserRole, ActualReturnedUserRole);
-        System.out.println(ActualReturnedUserRole);
-
-        **/
+        //String ExpectedReturnedUserRole = "Admin";     // UserRoleColumn
+        //String ActualReturnedUserRole = driver.findElement(By.xpath("//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[3]/div")).getText();
+        //Assert.assertEquals(ExpectedReturnedUserRole, ActualReturnedUserRole);
+        //System.out.println(ActualReturnedUserRole);
 
 
         /** //User : Alic Duval is inconsistent in the OrangeHRM platform
-        String ExpectedReturnedEmployeeName = "Alice Duval";
-        String ActualReturnedEmployeeName = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[5]/div")).getText();
-        Assert.assertEquals(ExpectedReturnedEmployeeName, ActualReturnedEmployeeName);
-        System.out.println(ActualReturnedEmployeeName);
+         String ExpectedReturnedEmployeeName = "Alice Duval";
+         String ActualReturnedEmployeeName = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[5]/div")).getText();
+         Assert.assertEquals(ExpectedReturnedEmployeeName, ActualReturnedEmployeeName);
+         System.out.println(ActualReturnedEmployeeName);
 
-        String ExpectedReturnedStatus = "Enabled";
-        String ActualReturnedStatus = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[5]/div")).getText();
-        Assert.assertEquals(ExpectedReturnedStatus, ActualReturnedStatus);
-        System.out.println(ActualReturnedStatus);
-        **/
+         String ExpectedReturnedStatus = "Enabled";
+         String ActualReturnedStatus = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[5]/div")).getText();
+         Assert.assertEquals(ExpectedReturnedStatus, ActualReturnedStatus);
+         System.out.println(ActualReturnedStatus);
+         **/
         driver.quit();
     }
 }
